1a51cebfc2474666f0734ea19bc95007b4fca459
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-12-20 21:49:55 +0200

    rewrote to webflux

--- backend/src/main/java/api/WikiStatsAPI.java
+++ backend/src/main/java/api/WikiStatsAPI.java
@@ -1,24 +1,26 @@
 package api;
 
-import api.controllers.users.UsersController;
-import reactor.netty.DisposableServer;
-import reactor.netty.http.server.HttpServer;
-import reactor.netty.http.server.HttpServerRoutes;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
-public class WikiStatsAPI {
+import java.time.LocalTime;
+
+@SpringBootApplication
+@Component
+public class WikiStatsAPI  implements ApplicationRunner {
     public static void main(String[] args) {
-        UsersController usersController = new UsersController();
+        SpringApplication.run(WikiStatsAPI.class, args);
+    }
 
-        DisposableServer server = HttpServer
-                .create()
-                .host("localhost")
-                .port(8000)
-                .route(routes ->
-                    routes.get("/users", usersController::getUsersRoute))
-                .bindNow();
+    @Override
+    public void run(ApplicationArguments arg0) throws Exception {
 
-        server
-            .onDispose()
-            .block();
     }
 }
--- backend/src/main/java/api/controllers/users/UsersController.java
+++ backend/src/main/java/api/controllers/users/UsersController.java
@@ -1,31 +1,24 @@
 package api.controllers.users;
 
-import api.utils.PagingInfoConstructor;
 import application.users.getlist.GetUsersListQuery;
 import application.users.getlist.GetUsersListQueryHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
-import reactor.netty.http.server.HttpServerRequest;
-import reactor.netty.http.server.HttpServerResponse;
+import application.users.getlist.UserDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-
+@RestController
+@RequestMapping("/users")
 public class UsersController {
-    public Publisher<Void> getUsersRoute(HttpServerRequest request, HttpServerResponse response) {
-        var paging = new PagingInfoConstructor(request);
-        var usersFlux = new GetUsersListQueryHandler().execute(new GetUsersListQuery(paging.page, paging.count));
-
-        var users = usersFlux.buffer().single().share().block();
-
-        try {
-            var mapper = new ObjectMapper();
-            var str = mapper.writeValueAsString(users);
-            response.header("content-type", "application/json");
-            return response.sendString(Mono.just(str));
-        } catch (JsonProcessingException ex) {
-            return response.status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
-        }
+    @GetMapping("")
+    private Flux<UserDto> getAll(
+            @RequestParam("page") Integer page,
+            @RequestParam("count") Integer count
+    ) {
+        var query = new GetUsersListQuery(page, count);
+        return new GetUsersListQueryHandler().execute(query);
     }
 }
+
